1f524ba5ed862f62cb2b783f72ce1fc69f6f66cb
Shumilova Tatyana <star28@inbox.ru>
2022-09-05 19:51:55 +0300

    Добавлены решения задач по ДЗ2

---
+++ Task15.py
@@ -0,0 +1,24 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# Пример:
+
+# - 6782 -> 23
+# - 0,56 -> 11
+
+# num = int(input("Введите число:"))
+# print(num)
+# s = str(num)
+# sum = 0
+# for i in range(len(s)):
+#     sum = sum + int(s[i])
+# print('Сумма цифр числа', num,' = ', sum)   
+
+
+# float_num = input('Введите число: ')
+# sum = 0
+# for i in float_num:
+#     if i != '.':
+#         sum = sum + int(i)
+# print('Сумма цифр числа', float_num,' = ', sum)
+
+
