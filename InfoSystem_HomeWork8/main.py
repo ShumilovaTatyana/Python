5e23fbd1318c4f4176b560d7ecb1a7f95dd31764
Shumilova Tatyana <star28@inbox.ru>
2022-10-03 12:52:39 +0300

    add requirements.txt

--- InfoSystem_HomeWork8/main.py
+++ InfoSystem_HomeWork8/main.py
@@ -1,3 +1,31 @@
+# Выберите необходимое действие
+# 1. Найти сотрудника
+# 2. Сделать выборку сотрудников по должности
+# 3. Сделать выборку сотрудников по зарплате
+# 4. Добавить сотрудника
+# 5. Удалить сотрудника
+# 6. Обновить данные сотрудника
+# 7. Экспортировать данные в формате json
+# 8. Экспортировать данные в формате cmv
+# 9. Закончить работу
+
+# def find_employees_by_salary_range(employees: list) -> list:
+#     result = []
+#     lo, hi = get_salary_range()
+#     for employee in employees:
+#         if lo <= employee["salary"] <= hi:
+#             result.append(employee)
+#     return result
+
+# def find_employees_by_salary_range(list_of_salary):
+#     result = []
+#     lo, hi = 1000, 3000
+#     for i in range(len(list_of_salary)):
+#         if lo <= list_of_salary[i] <= hi:
+#             result.append(list_of_salary[i])
+#     return result
+
+
 from controller import work_with_data_base
 
 if __name__ == "__main__":
---
+++ Sem9.1.py
@@ -0,0 +1,28 @@
+#Код от преподавателя с семинара
+
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from random import randint
+
+bot_token="5562619788:AAF8G5GFgRvf6A8vZ34bthvSOXBaI8EkXdM"
+bot = Bot(bot_token)
+updater = Updater(bot_token, use_context=True)
+dispatcher = updater.dispatcher
+def start(update, context):
+    context.bot.send_message(update.effective_chat.id, "Привет!")
+
+start_handler = CommandHandler('start', start)
+def info(update, context):
+    context.bot.send_message(update.effective_chat.id, "это бот")
+def roll(update, context):
+    context.bot.send_message(update.effective_chat.id, text=str(randint(1, 6)))
+
+start_handler = CommandHandler('start', start)
+info_handler = CommandHandler('info', info)
+roll_handler = CommandHandler('roll', roll)
+
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(info_handler)
+dispatcher.add_handler(roll_handler)
+updater.start_polling()
+updater.idle()
